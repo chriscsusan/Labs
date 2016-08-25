b9cdd09c13cefe52e9ca22233ad85bcdcae37968
Chris Susan <chriscsusan@gmail.com>
2016-08-25 13:20:12 -0400

    first commit

---
+++ src/Lab1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String proceed = "y";
+		
+		while (proceed.equalsIgnoreCase("y")) {
+		
+			System.out.println("Welcome to Grand Circus' Room Detail Generator!");
+			System.out.println("Please enter the length:");
+			
+			double length = scanner.nextDouble();		
+			System.out.println("Please enter the width:");
+			double width = scanner.nextDouble();		
+			System.out.println("Please enter the height:");
+			double height = scanner.nextDouble();
+		
+			double area = width * length;
+			double volume = width * length * height;
+			double perimeter = 2 * (width + length);
+		
+			System.out.println("The area of the room is " + area);
+			System.out.println("The perimeter of the room is " + perimeter);
+			System.out.println("The volume of the room is " + volume);
+		
+			System.out.println("Continue? (y/n)");		
+			proceed = scanner.next();
+			System.out.println();
+		}
+		System.out.println("Fin.");
+		scanner.close();
+	}
+}
