bd75f2dc1a690b3d3fd8742710e17f44e3a5a46f
Chris Susan <chriscsusan@gmail.com>
2016-09-06 17:31:04 -0400

    Adding Lab 10 (Movie List)

---
+++ src/labs/Movie.java
@@ -0,0 +1,19 @@
+package labs;
+
+public class Movie {
+	private String title;
+	private String category;
+	
+	
+	public Movie(String title, String category){
+		this.title = title;
+		this.category = category;
+	}
+
+	public String getTitle(){
+		return this.title;
+	}
+	public String getCategory(){
+		return this.category;
+	}
+}
