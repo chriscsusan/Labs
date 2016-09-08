98109ab380712ccde06a6787739fda74c92c3473
Chris Susan <chriscsusan@gmail.com>
2016-09-08 11:49:00 -0400

    Adding Lab12 sans Validator class

---
+++ src/labs/AlwaysRock.java
@@ -0,0 +1,9 @@
+package labs;
+
+public class AlwaysRock extends Player {
+
+	@Override
+	public Roshambo generateRoshambo(){
+		return Roshambo.ROCK;
+	}
+}
---
+++ src/labs/Player.java
@@ -0,0 +1,22 @@
+package labs;
+
+public class Player {
+
+	private String name;
+	int wins = 0;
+	int losses = 0;
+	int ties = 0;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public Roshambo generateRoshambo(){
+		
+		return Roshambo.ROCK;
+	}
+}
