d57fd2bb8ef4361c3c6667b52dfeb2dc014b3a18
Chris Susan <chriscsusan@gmail.com>
2016-09-14 10:26:59 -0400

    Added recursion to Lab4

--- src/labs/Lab4.java
+++ src/labs/Lab4.java
@@ -9,22 +9,30 @@ public class Lab4 {
 		long fac = 1;
 		
 		while (proceed.equalsIgnoreCase("y")){
-			System.out.println("Enter an integer from 1 to 10.");
+			System.out.println("Enter an integer.");
 			int num = scanner.nextInt();
 			
-			if (num > 0 && num <=10){
-				fac = 1;
-				for (int i = 1; i <= num; i++){
-				fac *= i;
-				}
-				System.out.println("Result is:" + fac);
-			}else{
+//			if (num > 0 && num <=10){
+//				fac = 1;
+//				for (int i = 1; i <= num; i++){
+//				fac *= i;
+//				}
+				System.out.println("Result is:" + calculateFactorial(num));
+//			}else{
 				
-			System.out.println("Entered number is not between 1 and 10.");
-			}
+//			System.out.println("Entered number is not between 1 and 10.");
+//			}
 			System.out.println("Continue? (y/n)");
 			proceed = scanner.next();
 		}
 		scanner.close();
+	}
+	
+	public static int calculateFactorial(int n){
+		
+		  if (n!=0){
+			    return n*calculateFactorial(n-1);
+			  }  
+			  return 1;
 	}
 }
