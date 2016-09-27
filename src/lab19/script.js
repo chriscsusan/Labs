aaca8aa38d59a4174a8710371f7330cd9f4681c4
Chris Susan <chriscsusan@gmail.com>
2016-09-27 10:26:51 -0400

    Added code to show total price in page

--- src/lab19/script.js
+++ src/lab19/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var cart = [];
 
 	$('.add-button').click(function() {
-
+		
 		row = $(this).parent();
 		price = row.children(".price").text();
 		name = row.children(".name").text();
@@ -19,6 +19,17 @@ $(document).ready(function() {
 			total += Number(cart[i].price);
 		}
 		console.log("Total cost: " + parseFloat(Math.round(total * 100) / 100).toFixed(2));
+		
+		
+		var displayTotal = document.createElement('p');
+		$(displayTotal).attr('id', 'total');
+//		for (var i = 0; i < cart.lenth; i++){
+//			$(displayTotal).html("<ul><li>" + cart[i].name + " $" + cart[i].price + "</li></ul>");
+//			$("total").append(displayTotal);
+//		}
+		$(displayTotal).html("Your total is: " + total);
+		$("body").append(displayTotal);
+		
 	});
 
 });
